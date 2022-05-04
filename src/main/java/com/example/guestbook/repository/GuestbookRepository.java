9533cf105ab626cd292886c63b4502b134ceb5f0
seonggon do <62453668+mooh2jj@users.noreply.github.com>
2022-05-04 22:58:39 +0900

    findByTest 진행

--- src/main/java/com/example/guestbook/repository/GuestbookRepository.java
+++ src/main/java/com/example/guestbook/repository/GuestbookRepository.java
@@ -2,9 +2,17 @@ package com.example.guestbook.repository;
 
 import com.example.guestbook.entity.Guestbook;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface GuestbookRepository extends JpaRepository<Guestbook, Long>,
         QuerydslPredicateExecutor<Guestbook> {
+
+//    @Override
+//    @Query("select gb from Guestbook gb where gb.id =:id ")
+//    Optional<Guestbook> findById(@Param("id") Long id);
 }
 
--- src/test/java/com/example/guestbook/repository/GuestbookRepositoryTest.java
+++ src/test/java/com/example/guestbook/repository/GuestbookRepositoryTest.java
@@ -2,13 +2,16 @@ package com.example.guestbook.repository;
 
 import com.example.guestbook.entity.Guestbook;
 import lombok.extern.slf4j.Slf4j;
-import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import java.util.List;
 import java.util.stream.LongStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @Slf4j
 @DataJpaTest
@@ -16,8 +19,16 @@ class GuestbookRepositoryTest {
 
     @Autowired
     private GuestbookRepository guestbookRepository;
-
     private Guestbook guestbook;
+
+    @BeforeEach
+    void setup() {
+        guestbook = Guestbook.builder()
+                .title("123L_title")
+                .content("123L_content")
+                .writer("123L_writer")
+                .build();
+    }
 
     @Test
     void create() {
@@ -33,7 +44,22 @@ class GuestbookRepositoryTest {
         List<Guestbook> guestbooks = guestbookRepository.findAll();
         log.info("guestbooks: {}", guestbooks);
 
-        Assertions.assertThat(guestbooks).isNotNull();
-        Assertions.assertThat(guestbooks.size()).isEqualTo(20);
+        assertThat(guestbooks).isNotNull();
+        assertThat(guestbooks.size()).isEqualTo(20);
+    }
+
+    @Test
+    public void findById(){
+
+        Guestbook savedbook = guestbookRepository.save(guestbook);
+
+        Guestbook findGuestbook = guestbookRepository.findById(savedbook.getId()).get();
+        findGuestbook.changeTitle("new_title");
+        findGuestbook.changeContent("new_content");
+
+        log.info("findGuestbook : {}",findGuestbook);
+
+        assertThat(findGuestbook.getTitle()).isEqualTo("new_title");
+        assertThat(findGuestbook.getContent()).isEqualTo("new_content");
     }
 }
